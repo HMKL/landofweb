6677c7500bf1a3f91a5a4fde77390196dce5a0ef
Catalin B <catalin.bac@gmail.com>
2022-08-09 10:29:38 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
+const getBtn = document.querySelector('.toggle-button');
+const getDiv = document.querySelector('.main');
+const width = window.matchMedia('(max-width:769px)');
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
 
-const width = window.matchMedia('(max-width:769px)');
-
-function checkWidth(e) {
-  if (e.matches) {
-    console.log('da');
+let count = 0;
+getBtn.addEventListener('click', () => {
+  count += 1;
+  if (count === 1) {
+    getDiv.style.display = 'none';
   } else {
-    console.log('nu');
+    getDiv.style.display = 'block';
+    count -= 2;
   }
-}
-checkWidth(width);
-width.addEventListener('change', checkWidth);
+});
+
+// function checkWidth(e) {
+//   if (e.matches) {
+//     console.log('da');
+//   } else {
+//     console.log('nu');
+//   }
+// }
+// checkWidth(width);
+// width.addEventListener('change', checkWidth);
